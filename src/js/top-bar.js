42a7b884209793b76af487a4cf498983f600f255
likogan <me@likogan.dev>
2023-08-01 17:29:33 -0600

    Move index.html CSS & JS to files & remove empty file smoothscroll.js

---
+++ src/js/top-bar.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
